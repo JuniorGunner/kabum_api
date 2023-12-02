916572740878a89a73c611a4d8efe89caabd6287
JuniorGunner <csf.junior90@gmail.com>
2023-12-02 07:47:27 -0300

    models: added freight options model

---
+++ models/freight_options.py
@@ -0,0 +1,19 @@
+
+FREIGHT_OPTIONS = {
+    "Entrega Ninja": {
+        "const": 0.3,
+        "min_height": 10,
+        "max_height": 200,
+        "min_width": 6,
+        "max_width": 140,
+        "delivery_days": 6
+    },
+    "Entrega KaBuM": {
+        "const": 0.2,
+        "min_height": 5,
+        "max_height": 140,
+        "min_width": 13,
+        "max_width": 125,
+        "delivery_days": 4 
+    }
+}
